85480c2cffe0e67767df0f82b5ba22369f60e438
jpeterson <jpeterson@sensei.ag>
2022-03-08 16:55:48 -0800

    advent_12 part 2 complete

--- .venv/advent_12.py
+++ .venv/advent_12.py
@@ -19,6 +19,8 @@ for cave in caves:
     else: 
         graph[cave[1]].append(cave[0])
 
+small_caves = [i for i in graph if i.islower() and (i != 'start' and i != 'end')]
+
 # paths = []
 # def dfs(graph=graph, paths=paths):
 #     path = []
@@ -53,15 +55,41 @@ def find_all_paths(graph=graph, start='start', end='end', path=[]):
                 paths.append(newpath)
     return paths
 
+def find_all_paths_part2(graph=graph, start='start', end='end', path=[], double_cave=''):
+    path = path + [start]
+    if start == end:
+        return [path]
+    if start not in graph:
+        return []
+    paths = []
+    for cave in graph[start]:
+        if cave not in path or cave.isupper() or (cave == double_cave and path.count(cave) < 2):
+            newpaths = find_all_paths_part2(graph, cave, end, path, double_cave)
+            for newpath in newpaths:
+                paths.append(newpath)
+    return paths
+
 
 pprint.pprint(graph)
 
 
 def main():
+#PART 1 
     # dfs()
     # print(paths)
     paths = find_all_paths()
-    print(len(paths))
+    # print(len(paths))
+
+# PART 2
+    paths_2 = set()
+    for small_cave in small_caves:
+        pths = find_all_paths_part2(graph,'start','end', [], small_cave)
+        for i in pths:
+            paths_2.add(tuple(i))
+    
+
+    # pprint.pprint(paths_2)
+    print(len(paths_2))
 
 
 if __name__ == "__main__":
