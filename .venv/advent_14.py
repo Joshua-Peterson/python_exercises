f1c680fb39da56a7567e3d8395ed9792441aa561
jpeterson <jpeterson@sensei.ag>
2022-03-16 14:52:49 -0700

    advent_14 complete

--- .venv/advent_14.py
+++ .venv/advent_14.py
@@ -8,11 +8,18 @@ with open(input) as file:
     rules_list = [i.split(' -> ') for i in rules_list]
     rules = {i[0]:i[1] for i in rules_list}
 
-pprint.pprint(rules)
+rules_pairs = {i:[i[0]+rules[i], rules[i]+i[1]] for i in rules}
+pair_counts = {i:0 for i in rules}
 example_polymer = "NNCB"
 polymer = "FSKBVOSKPCPPHVOPVFPC"
+letter_counts = {i:polymer.count(i) for i in set(rules.values())}
+steps = 40
 
-def build_polymer(polymer=polymer, rules=rules, steps=10):
+for i in range(len(polymer)-1):
+    pair = polymer[i] + polymer[i+1]
+    pair_counts[pair] += 1
+
+def build_polymer(polymer=example_polymer, rules=rules, steps=10):
     new_polymer = ''
     last = polymer[-1]
     for i in range(len(polymer)-1):
@@ -27,14 +34,41 @@ def build_polymer(polymer=polymer, rules=rules, steps=10):
        return build_polymer(new_polymer,rules,steps)
     return new_polymer
 
+def pair_counting(pair_counts=pair_counts,rules_pairs=rules_pairs, polymer=polymer, steps=steps, letter_counts=letter_counts, rules=rules):
+    for i in range(steps):
+        new_count = {i:0 for i in pair_counts}
+        for k, v in pair_counts.items():
+            new_count[rules_pairs[k][0]] += v
+            new_count[rules_pairs[k][1]] += v
+            letter_counts[rules[k]] += v
+
+        for k in pair_counts:
+            pair_counts[k] = new_count[k]
+
+
+    
+
+
+
 
 def main():
-    final_poly = build_polymer()
-    print(len(final_poly))
-    char_counts = {i : final_poly.count(i) for i in set(final_poly)}
-    print(char_counts)
-    part_1 = max(char_counts.values()) - min(char_counts.values())
-    print(part_1)
+    
+    #PART 1
+    # final_poly = build_polymer()
+    # print(len(final_poly))
+    # char_counts = {i : final_poly.count(i) for i in set(final_poly)}
+    # print(char_counts)
+    # part_1 = max(char_counts.values()) - min(char_counts.values())
+    # print(part_1)
+
+
+    #PART 2
+    pair_counting()
+    pprint.pprint(letter_counts)
+    print(max(letter_counts.values())-min(letter_counts.values()))
+
+   
+    
 
 
 if __name__ == "__main__":
